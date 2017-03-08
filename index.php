269a99d0cdda3dd4a45f19f7492309ebf090a6ad
tarmoteder <tarmo.teder@tptlive.ee>
2017-03-08 13:01:42 +0200

    Panime PHP failid juurde

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$date ="08.03.2017";
+$number = 5;
+$float = 5.2;
+$float1 = 5.18;
+
+echo "08.03.2017, kolmas tund<br>";
+
+echo 'Esimene float on '.$float." ja teine on ". $float1.'<br>';
+
+echo $float + $float1;
+
+?>
